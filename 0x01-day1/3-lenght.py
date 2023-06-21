23ddd4e6ab33c83490ddb87d7248378700097712
Souheila <souheila.sidennas@gmail.com>
2023-06-21 08:07:02 +0100

    With and without variables

--- 0x01-day1/3-lenght.py
+++ 0x01-day1/3-lenght.py
@@ -1,2 +1,6 @@
+# Using a variable
+name = input("What is your name? ")
+print(len(name))
+
 # the use of 3 functions to print a lenght of a string
-print(len(input("What is your name? ")))
+# print(len(input("What is your name? ")))
