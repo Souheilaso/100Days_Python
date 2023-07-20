83b31668e01e0287f6ef6314920e85a93b014290
Souheilaso <souheila.sidennas@gmail.com>
2023-07-20 17:19:02 +0100

    Primary Pizza App model

---
+++ 0x01_day4/PizzaApp.py
@@ -0,0 +1,25 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+######### Pizza Prices##########
+small_pizza = 15
+medium_pizza = 20
+large_pizza = 25
+######## Additions###########
+pepperoni_s = 2
+Pepperoni_m_l = 3
+Cheese = 1
+############ code##########
+
+if size.upper() == "S":
+    if add_pepperoni.lower() == "Y":
+        if extra_cheese.lower() == "Y":
+            print("Your total bill is ", small_pizza + pepperoni_s + Cheese)
+        else:
+            print("Your total bill is ", small_pizza + pepperoni_s)
+    else:
+        print("Your total bill is ", small_pizza)
+else:
+    print("choose the right size again please")
