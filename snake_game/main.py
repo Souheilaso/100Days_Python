274eb8eb39eb80c68c8018cd88be5fb0a90a6f1c
Souheilaso <souheila.sidennas@gmail.com>
2023-08-06 13:31:22 +0100

    connecting segments together through creating a new_x and a new_y

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,10 +1,11 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-
+screen.tracer(0)
 
 # def snakes(x, y):
 #     snake = Turtle("square")
@@ -16,11 +17,23 @@ screen.title("Snake Game")
 # snake1 = snakes(0, 0)
 # snake2 = snakes(-20, 0)
 # snake3 = snakes(-40, 0)
+snakes = []
 
 snake_position = [(0, 0), (-20, 0), (-40, 0)]
 for position in snake_position:
     snake = Turtle("square")
     snake.color("white")
+    snake.penup()
     snake.goto(position)
+    snakes.append(snake)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+    for snake_num in range(len(snakes) - 1, 0, -1):
+        new_x = snakes[snake_num - 1].xcor()
+        new_y = snakes[snake_num - 1].ycor()
+        snakes[snake_num].goto(new_x, new_y)
 
 screen.exitonclick()
