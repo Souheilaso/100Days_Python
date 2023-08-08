ace063e2f75a2565b814330da6e666fdfca34810
Souheilaso <souheila.sidennas@gmail.com>
2023-08-08 07:57:49 +0100

    Adding food class + inheritance

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,34 +9,20 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-# def snakes(x, y):
-#     snake = Turtle("square")
-#     snake.color("white")
-#     snake.goto(x, y)
-#     return snake
-#
-#
-# snake1 = snakes(0, 0)
-# snake2 = snakes(-20, 0)
-# snake3 = snakes(-40, 0)
-# snakes = []
-#
-# snake_position = [(0, 0), (-20, 0), (-40, 0)]
-# for position in snake_position:
-#     snake = Turtle("square")
-#     snake.color("white")
-#     snake.penup()
-#     snake.goto(position)
-#     snakes.append(snake)
 snake = Snake()
+food = Food()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.2)
-    # for snake_num in range(len(snakes) - 1, 0, -1):
-    #     new_x = snakes[snake_num - 1].xcor()
-    #     new_y = snakes[snake_num - 1].ycor()
-    #     snakes[snake_num].goto(new_x, new_y)
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
 screen.exitonclick()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 SNAKE_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 
 class Snake:
@@ -9,6 +13,7 @@ class Snake:
 
         self.snakes = []
         self.create_snake()
+        self.head = self.snakes[0]
 
     def create_snake(self):
         for position in SNAKE_POSITION:
@@ -23,4 +28,20 @@ class Snake:
             new_x = self.snakes[snake_num - 1].xcor()
             new_y = self.snakes[snake_num - 1].ycor()
             self.snakes[snake_num].goto(new_x, new_y)
-        self.snakes[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
