532ffabee0e07e04c1a7c032654fd6ce620ab4e8
Souheilaso <souheila.sidennas@gmail.com>
2023-08-10 08:40:59 +0100

    Setting up the screen

---
+++ Ping pong/main.py
@@ -0,0 +1,8 @@
+from turtle import Screen, Turtle
+t = Turtle()
+t.color("white")
+screen = Screen()
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+screen.exitonclick()
