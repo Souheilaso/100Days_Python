33bc8c92cb2e4b80643410842c25d7014dcb045b
Souheilaso <souheila.sidennas@gmail.com>
2023-08-10 10:12:46 +0100

    Updating onkey -> s, c for left and Up, Down for right paddle

--- Ping pong/main.py
+++ Ping pong/main.py
@@ -7,30 +7,14 @@ screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
 
-# paddle = Turtle("square")
-# paddle.shapesize(stretch_wid=5, stretch_len=1)
-# paddle.color("white")
-# paddle.penup()
-# paddle.goto(x=350, y=0)
-# screen.listen()
-#
-#
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor(), new_y)
-#
-#
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor(), new_y)
 screen.listen()
 r_paddle = Paddle(350, 0)
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 
 l_paddle = Paddle(-350, 0)
-screen.onkey(l_paddle.go_up, "Up")
-screen.onkey(l_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "s")
+screen.onkey(l_paddle.go_down, "c")
 
 game_is_on = True
 while game_is_on:
