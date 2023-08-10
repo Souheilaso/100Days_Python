1e6894925370cbb6f28940762ef28c37474fd342
Souheilaso <souheila.sidennas@gmail.com>
2023-08-10 10:17:11 +0100

    inhereting Turtle class and updating self.paddle to self.

--- Ping pong/paddle.py
+++ Ping pong/paddle.py
@@ -1,18 +1,19 @@
 from turtle import Turtle
 
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, x, y):
-        self.paddle = Turtle("square")
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.color("white")
-        self.paddle.penup()
-        self.paddle.goto(x, y)
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(x, y)
 
     def go_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.sety(new_y)
+        new_y = self.ycor() + 20
+        self.sety(new_y)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.sety(new_y)
+        new_y = self.ycor() - 20
+        self.sety(new_y)
