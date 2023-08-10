95037489466d2d3503ecf029df3f8c5ebc22107d
Souheilaso <souheila.sidennas@gmail.com>
2023-08-10 10:05:40 +0100

    Applying paddle class to create the right paddle

--- Ping pong/main.py
+++ Ping pong/main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(height=600, width=800)
@@ -6,26 +7,28 @@ screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
 
-paddle = Turtle("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color("white")
-paddle.penup()
-paddle.goto(x=350, y=0)
+# paddle = Turtle("square")
+# paddle.shapesize(stretch_wid=5, stretch_len=1)
+# paddle.color("white")
+# paddle.penup()
+# paddle.goto(x=350, y=0)
+# screen.listen()
+#
+#
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor(), new_y)
+#
+#
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor(), new_y)
+
+r_paddle = Paddle(350, 0)
 screen.listen()
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
