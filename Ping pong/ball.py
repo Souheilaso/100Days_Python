d608aa2c323398197f1fd0219135da1b7ce535a4
Souheilaso <souheila.sidennas@gmail.com>
2023-08-11 08:20:32 +0100

    Creating a move function for the ball based on its xcor, ycor

--- Ping pong/ball.py
+++ Ping pong/ball.py
@@ -12,3 +12,7 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.goto(x=0, y=0)
 
+    def move(self):
+        new_x = self.xcor() + XMOVE
+        new_y = self.ycor() + YMOVE
+        self.goto(new_x, new_y)
