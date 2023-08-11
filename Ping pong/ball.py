bd7d7169c6d8cea9bd6c7a8807a5add9b68d321b
Souheilaso <souheila.sidennas@gmail.com>
2023-08-11 08:40:59 +0100

    creating a new x_move and y_move attributes

--- Ping pong/ball.py
+++ Ping pong/ball.py
@@ -1,8 +1,4 @@
 from turtle import Turtle
-XMOVE = 10
-YMOVE = 10
-
-
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
@@ -11,8 +7,11 @@ class Ball(Turtle):
         self.penup()
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.goto(x=0, y=0)
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + XMOVE
-        new_y = self.ycor() + YMOVE
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
