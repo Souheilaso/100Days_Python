96fa2430a470d5dc792c2a96c6ddce67acdb139a
Souheilaso <souheila.sidennas@gmail.com>
2023-08-11 10:02:09 +0100

    re organizing the code. adding update score and lpoint

--- Ping pong/scoreboard.py
+++ Ping pong/scoreboard.py
@@ -11,8 +11,15 @@ class Score(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align=ALIGNMENT, font=FONT)
         self.goto(100, 200)
         self.write(self.r_score, align=ALIGNMENT, font=FONT)
+
+    def l_point(self):
+        self.l_score += 1
 
