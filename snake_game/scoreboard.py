f19b4b792e13e51c28afcc7ae1fd855136e69409
Souheilaso <souheila.sidennas@gmail.com>
2023-08-18 07:05:44 +0100

    commiting game_over to change it with reset

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -18,9 +18,9 @@ class Score(Turtle):
         self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
