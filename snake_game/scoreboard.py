6d7db55f1c74d40abe5a4289aae07cf727c09134
Souheilaso <souheila.sidennas@gmail.com>
2023-08-18 13:24:35 +0100

    reading and writing highest score

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,7 +7,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -21,6 +22,8 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
