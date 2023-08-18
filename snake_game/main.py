97ddef910053055f8f4ae0303afddc9497bf286f
Souheilaso <souheila.sidennas@gmail.com>
2023-08-18 07:15:56 +0100

    adding the reset method

--- snake_game/main.py
+++ snake_game/main.py
@@ -31,13 +31,13 @@ while game_is_on:
         score.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
 
-    for segment in snake.snakes[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+    for segment in snake.snakes:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            score.reset()
 
 
 screen.exitonclick()
