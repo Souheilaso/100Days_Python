6326b8e1bfa27ba25e02dc0b31d1f832c33f868a
Souheilaso <souheila.sidennas@gmail.com>
2023-08-18 07:11:19 +0100

    creating reset method

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -16,7 +16,13 @@ class Score(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
     # def game_over(self):
     #     self.goto(0, 0)
