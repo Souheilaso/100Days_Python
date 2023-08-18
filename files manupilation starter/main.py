a4fe3cefaff72e8b1292f7b3097b53b750b31586
Souheilaso <souheila.sidennas@gmail.com>
2023-08-18 13:53:21 +0100

    testing absolute path

--- files manupilation starter/main.py
+++ files manupilation starter/main.py
@@ -1,10 +1,10 @@
-with open("my_file.txt") as file:
+with open("/Users/sue/Desktop/my_file.txt") as file:
     content = file.read()
     print(content)
     file.close()
 
-with open("my_file.txt", mode="a") as file:
-    file.write("\n I am happy")
+with open("/Users/sue/Desktop/my_file.txt", mode="a") as file:
+    file.write("\ntesting absolute path")
 
 with open("new_file.text", mode="w") as file:
     file.write("Hello")
