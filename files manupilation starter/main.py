171145778c2ed1e447efc7097b29ba80f828b942
Souheilaso <souheila.sidennas@gmail.com>
2023-08-24 11:02:31 +0100

    mail merge

--- files manupilation starter/main.py
+++ files manupilation starter/main.py
@@ -1,10 +1,9 @@
-with open("/Users/sue/Desktop/my_file.txt") as file:
+with open("../../../../Desktop/my_file.txt") as file:
     content = file.read()
     print(content)
-    file.close()
 
-with open("/Users/sue/Desktop/my_file.txt", mode="a") as file:
-    file.write("\ntesting absolute path")
-
-with open("new_file.text", mode="w") as file:
-    file.write("Hello")
+# with open("/Users/sue/Desktop/my_file.txt", mode="a") as file:
+#     file.write("\ntesting absolute path")
+#
+# with open("new_file.text", mode="w") as file:
+#     file.write("Hello")
