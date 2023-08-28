29e8773a0bded55b904da5161d8e204b5c941b1d
Souheilaso <souheila.sidennas@gmail.com>
2023-08-28 09:06:18 +0100

    Calculating average with two methods. The best is using import statistics

--- CSV_data/main.py
+++ CSV_data/main.py
@@ -18,4 +18,8 @@ data_dict = data.to_dict()
 print(data_dict)
 
 temp_list = data["temp"].to_list()
-print(temp_list)
+average = sum(temp_list) / len(temp_list)
+print(average)
+
+import statistics
+print(statistics.mean(temp_list))
