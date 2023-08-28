a935bdb4f84b945ae5f0af88c8050f2cbbe4a4ae
Souheilaso <souheila.sidennas@gmail.com>
2023-08-28 09:08:47 +0100

    Calculating average with pandas using series.mean()

--- CSV_data/main.py
+++ CSV_data/main.py
@@ -18,8 +18,12 @@ data_dict = data.to_dict()
 print(data_dict)
 
 temp_list = data["temp"].to_list()
-average = sum(temp_list) / len(temp_list)
-print(average)
+# average = sum(temp_list) / len(temp_list)
+# print(average)
 
-import statistics
-print(statistics.mean(temp_list))
+# import statistics
+# print(statistics.mean(temp_list))
+#
+
+average = data["temp"].mean()
+print(average)
