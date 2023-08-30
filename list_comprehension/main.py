810efa457f498fb14e27359dc5cc9a249b919f48
Souheilaso <souheila.sidennas@gmail.com>
2023-08-30 10:17:19 +0100

    creating a list

--- list_comprehension/main.py
+++ list_comprehension/main.py
@@ -5,3 +5,13 @@ print(squared_numbers)
 # reminder
 result = [n for n in numbers if n % 2 == 0]
 print(result)
+
+# reading both files then finding similar numbers and creating a list
+with open("file1.txt") as data_file1:
+    content1 = data_file1.read()
+
+with open("file2.txt") as data_file2:
+    content2 = data_file2.read()
+
+list_1 = [int(number) for number in content1.split()]
+list_2 = [int(number) for number in content2.split()]
