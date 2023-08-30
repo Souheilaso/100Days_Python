b51537412e481e31ec0886d4dbba8a32e233312e
Souheilaso <souheila.sidennas@gmail.com>
2023-08-30 14:13:05 +0100

    using an old dictionary to create a new one

--- dictionary_comprehension/main.py
+++ dictionary_comprehension/main.py
@@ -4,3 +4,21 @@ list_of_words = sentence.split()
 
 new_dict = {word: len(word) for word in list_of_words}
 print(new_dict)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+
+weather_f = {days: temperature * 9/5 + 32 for (days, temperature) in weather_c.items()}
+
+print(weather_f)
