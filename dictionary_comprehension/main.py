74c8737fc750fef40a8e16cfb83eff0d35710b57
Souheilaso <souheila.sidennas@gmail.com>
2023-08-30 13:52:59 +0100

    creating a dictionary

--- dictionary_comprehension/main.py
+++ dictionary_comprehension/main.py
@@ -2,4 +2,5 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 # converting  a sentence to a word
 list_of_words = sentence.split()
 
-
+new_dict = {word: len(word) for word in list_of_words}
+print(new_dict)
