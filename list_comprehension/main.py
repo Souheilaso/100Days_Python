1ad571d585c255ced1879ea6e279b7fee7a3752c
Souheilaso <souheila.sidennas@gmail.com>
2023-08-30 10:03:36 +0100

    list comprehension with reminder == even number

--- list_comprehension/main.py
+++ list_comprehension/main.py
@@ -1,3 +1,7 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squared_numbers = [number * number for number in numbers]
 print(squared_numbers)
+
+# reminder
+result = [n for n in numbers if n % 2 == 0]
+print(result)
