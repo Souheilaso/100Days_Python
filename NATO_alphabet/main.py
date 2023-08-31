b5d48536013607516acb7f141baddec5c55fecd5
Souheilaso <souheila.sidennas@gmail.com>
2023-08-31 09:29:52 +0100

    Reading csv file using pandas

--- NATO_alphabet/main.py
+++ NATO_alphabet/main.py
@@ -20,8 +20,11 @@
 # # Keyword Method with iterrows()
 # # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# TODO 1. read csv.
+import pandas
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+data = pandas.read_csv("nato_alphabets.csv")
+
+
+
 
