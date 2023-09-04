d3e9013654da0a1b4a94185838a324b0d16fbce0
Souheilaso <souheila.sidennas@gmail.com>
2023-09-04 07:38:19 +0100

    adding .pack(), nothing will show up if it is not there

--- tkinter_starter/main.py
+++ tkinter_starter/main.py
@@ -3,7 +3,8 @@ window = tkinter.Tk()
 window.title("First GUI")
 window.minsize(width=500, height=300)
 
-
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 
 
 
