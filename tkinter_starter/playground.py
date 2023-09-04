205e79c76acfd1403ff9274d0ad80bb900631121
Souheilaso <souheila.sidennas@gmail.com>
2023-09-04 08:39:50 +0100

    **kwargs printing n

--- tkinter_starter/playground.py
+++ tkinter_starter/playground.py
@@ -2,7 +2,7 @@ def add(*args):
     result = 0
     for n in args:
         result += n
-    print(result)
+    # print(result)
 
 
 add(1, 2)
@@ -11,5 +11,6 @@ add(1, 2)
 def calculate(n, **kwargs):
     n += kwargs["add"]
     n *= kwargs["multiply"]
+    print(n)
 
 calculate(2, add=3, multiply=5)
