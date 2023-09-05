8a4ade209881ae0da9069dbcba2ab02bdc6a5112
Souheilaso <souheila.sidennas@gmail.com>
2023-09-05 08:41:29 +0100

    creating a button

--- tkinter_starter/main.py
+++ tkinter_starter/main.py
@@ -6,10 +6,12 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-greet = tkinter.Label(text="Python Rocks", fg="red", background="white")
+greet = tkinter.Label(text="Python Rocks", fg="red")
 greet.pack()
 
-
+# buttons
+button = tkinter.Button(text="click me", height=3, width=20, foreground="red")
+button.pack()
 
 
 
