42ec7bd7964bb66e71a4c0a2bff9fa2722e1a1ae
Souheilaso <souheila.sidennas@gmail.com>
2023-09-05 08:52:44 +0100

    creating a function to use .get

--- tkinter_starter/main.py
+++ tkinter_starter/main.py
@@ -1,7 +1,15 @@
 import tkinter
+
+
+def save_print():
+    name = entry.get()
+    print(name)
+
+
 window = tkinter.Tk()
 window.title("First GUI")
 window.minsize(width=500, height=300)
+
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
@@ -10,14 +18,12 @@ greet = tkinter.Label(text="Python Rocks", fg="red")
 greet.pack()
 
 # buttons
-button = tkinter.Button(text="click me", height=3, width=20, foreground="red")
+button = tkinter.Button(text="Save", height=3, width=20, foreground="red", command=save_print)
 button.pack()
 
 # Entry
 entry = tkinter.Entry()
 entry.pack()
-
-
 
 
 window.mainloop()
