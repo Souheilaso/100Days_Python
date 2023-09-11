58a8ed5fbf9406f5606b0d212b8290f0df37137d
Souheilaso <souheila.sidennas@gmail.com>
2023-09-11 10:22:04 +0100

    using grid() instead of pack to organize the structure

--- tkinter_starter/main.py
+++ tkinter_starter/main.py
@@ -12,18 +12,19 @@ window.minsize(width=500, height=300)
 
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+# my_label.pack()
+my_label.grid(column=0, row=0)
 
 greet = tkinter.Label(text="Python Rocks", fg="red")
-greet.pack()
+# greet.pack()
 
 # buttons
 button = tkinter.Button(text="Save", height=3, width=20, foreground="red", command=save_print)
-button.pack()
+# button.pack()
 
 # Entry
 entry = tkinter.Entry()
-entry.pack()
+# entry.pack()
 
 
 window.mainloop()
