0d0ac16d6565c39b93a70848ca7f3220a26d586d
Souheilaso <souheila.sidennas@gmail.com>
2023-09-11 10:59:18 +0100

    creating the first label with grid

--- Mile_to_Km Converter TK/main.py
+++ Mile_to_Km Converter TK/main.py
@@ -8,3 +8,7 @@ window.minsize(width=500, height=300)
 # Creating an Entry
 entry = tkinter.Entry()
 entry.grid(column=1, row=0)
+
+# Creating label 1
+miles_label = tkinter.Label(text="Miles", font=("Arial", 24, "bold"))
+miles_label.grid(column=2, row=0)
