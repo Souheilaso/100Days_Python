35525077b7d829c8e154b11ef0562e80d0cadd99
Souheilaso <souheila.sidennas@gmail.com>
2023-09-11 10:25:07 +0100

    applying grid on entry

--- tkinter_starter/main.py
+++ tkinter_starter/main.py
@@ -17,16 +17,17 @@ my_label.grid(column=0, row=0)
 
 greet = tkinter.Label(text="Python Rocks", fg="red")
 # greet.pack()
+greet.grid(column=2, row=0)
 
 # buttons
 button = tkinter.Button(text="Save", height=3, width=20, foreground="red", command=save_print)
 button.grid(column=1, row=1)
 # button.pack()
 
-
 # Entry
 entry = tkinter.Entry()
 # entry.pack()
+entry.grid(column=3, row=2)
 
 
 window.mainloop()
