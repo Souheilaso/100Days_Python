86e5e14a8f2efe6069cd81547c64b7e4314fd59b
Souheilaso <souheila.sidennas@gmail.com>
2023-09-12 16:56:07 +0100

    setting a background and adding an image

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -21,13 +21,18 @@ import tkinter
 window = tkinter.Tk()
 # Window title
 window.title("POMODORO")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 # creating canvas
-canvas = tkinter.Canvas(width=200, height=224)
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 # finding the image using photoimage
 image = tkinter.PhotoImage(file="tomato.png")
 # creating the image
 canvas.create_image(100, 112, image=image)
+# Adding timing text
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+# layout
+canvas.pack()
 
 # Keep the window open
 window.mainloop()
