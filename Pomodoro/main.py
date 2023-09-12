c86514d3ff6be6519f95d00be36783052fb20582
Souheilaso <souheila.sidennas@gmail.com>
2023-09-12 17:11:29 +0100

    creating a label

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,3 +1,5 @@
+import tkinter
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,24 +17,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-import tkinter
 
-# create a window
+# Create a window
 window = tkinter.Tk()
 # Window title
 window.title("POMODORO")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-# creating canvas
+# Creating canvas
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-# finding the image using photoimage
+# Finding the image using PhotoImage
 image = tkinter.PhotoImage(file="tomato.png")
-# creating the image
+# Creating the image
 canvas.create_image(100, 112, image=image)
 # Adding timing text
 canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
-# layout
-canvas.pack()
+# Layout
+canvas.grid(column=1, row=1)  # Use grid instead of pack
+
+# Creating Timer label
+timer_label = tkinter.Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
 
 # Keep the window open
 window.mainloop()
+
