1d33cc5a25bc2d514b704d9cb699671b1ecf1e3f
Souheilaso <souheila.sidennas@gmail.com>
2023-09-13 08:18:50 +0100

    adding long break min

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -18,7 +18,8 @@ reps = 0
 def start_timer():
     global reps
     work_sec = WORK_MIN * 60
-    short_break_sec = LONG_BREAK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_min = LONG_BREAK_MIN * 60
     count_down(WORK_MIN * 60)  # Start with 25 minutes (25 * 60 seconds)
 
 
