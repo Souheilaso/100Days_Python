c35dc92e5d78705c3234740655b20692f9ab4549
Souheilaso <souheila.sidennas@gmail.com>
2023-09-13 08:17:29 +0100

    adding a global variable reps, adding work seconds in start_timer

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -9,12 +9,16 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
+    global reps
+    work_sec = WORK_MIN * 60
+    # short_break_sec = LONG_BREAK_MIN * 60
     count_down(WORK_MIN * 60)  # Start with 25 minutes (25 * 60 seconds)
 
 
