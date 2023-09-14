635d1d347247c2274b48a6f2daf537ea6728bfc1
Souheilaso <souheila.sidennas@gmail.com>
2023-09-14 09:06:53 +0100

    setting up canvas and editing padx, pady, width and height of canvas,window, create_image

--- password_manager/main.py
+++ password_manager/main.py
@@ -13,17 +13,17 @@ window = tkinter.Tk()
 
 # Window title
 window.title("Password Manager")
-window.config(padx=200, pady=200)
+window.config(padx=100, pady=100)
 
 # creating a canvas
-canvas = tkinter.Canvas(width=200, height=224)
+canvas = tkinter.Canvas(width=500, height=500)
 
 # Finding the image
 image = tkinter.PhotoImage(file="logo.png")
 
 # creating the image location in the canvas
-canvas.create_image(100, 112, image=image)
-
+canvas.create_image(250, 200, image=image)
+canvas.grid(column=1, row=0)
 
 
 
