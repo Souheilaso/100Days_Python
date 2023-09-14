7ba119aa65e5388a82f8047909eeb43237d2e7ca
Souheilaso <souheila.sidennas@gmail.com>
2023-09-14 08:49:15 +0100

    setting up the title and window.config

--- password_manager/main.py
+++ password_manager/main.py
@@ -11,5 +11,13 @@ import tkinter
 # create a window
 window = tkinter.Tk()
 
+# Window title
+window.title("Password Manager")
+window.config(padx=200, pady=200)
+
+
+
+
+
 # keep the window in a loop
 window.mainloop()
