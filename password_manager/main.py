13986af304e3cfe3f8a9fafab9dfe0ad7a76b7db
Souheilaso <souheila.sidennas@gmail.com>
2023-09-15 08:35:42 +0100

    updating the logo image and canvas

--- password_manager/main.py
+++ password_manager/main.py
@@ -13,16 +13,16 @@ window = tkinter.Tk()
 
 # Window title
 window.title("Password Manager")
-window.config(padx=100, pady=100)
+window.config(padx=30, pady=30)
 
 # creating a canvas
-canvas = tkinter.Canvas(width=500, height=500)
+canvas = tkinter.Canvas(width=200, height=200)
 
 # Finding the image
 image = tkinter.PhotoImage(file="logo.png")
 
 # creating the image location in the canvas
-canvas.create_image(250, 200, image=image)
+canvas.create_image(120, 100, image=image)
 canvas.grid(column=1, row=0)
 
 # Create Website Label
@@ -48,11 +48,12 @@ password_entry = tkinter.Entry()
 password_entry.grid(column=1, row=3)
 
 
-
 # create Generate Password Button
 password_btn = tkinter.Button(text="Generate Password")
+password_btn.grid(column=2, row=3)
 # Create Add Button
 add_btn = tkinter.Button(text="Add")
+add_btn.grid(column=1, row=4)
 
 
 
