7d0eeb7fe0e76185d19c03517c275901744d5744
Souheilaso <souheila.sidennas@gmail.com>
2023-09-20 16:15:08 +0100

    using finally, else, file manupilation

--- catching_exceptions/main.py
+++ catching_exceptions/main.py
@@ -1,9 +1,14 @@
 try:
-    file = open("a_file.txt")
+    with open("a_file.txt", "r") as file:
+        content = file.read()
     dictionary_key = {"key": "value"}
     print(dictionary_key["sdf"])
 except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("new line")
+    with open("a_file.txt", "w") as file:
+        file.write("new line\n")
 except KeyError as error_message:
     print(f"the {error_message} key does not exist.")
+else:
+    print(content)
+finally:
+    print("The file is closed")
