729efddbf40aabe6621687c50df9533e9fc4363b
Souheilaso <souheila.sidennas@gmail.com>
2023-09-20 16:06:32 +0100

    checking keyerrors

--- catching_exceptions/main.py
+++ catching_exceptions/main.py
@@ -1,5 +1,9 @@
 try:
     file = open("a_file.txt")
+    dictionary_key = {"key": "value"}
+    print(dictionary_key["sdf"])
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("new line")
+except KeyError as error_message:
+    print(f"the {error_message} key does not exist.")
